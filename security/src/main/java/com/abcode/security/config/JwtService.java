330b9980b89be2a46f2a731d23336dc36b55763b
Arthur Barbosa <arthurbruno03@gmail.com>
2023-01-20 13:27:22 -0300

    Created autencation and register v1

--- security/src/main/java/com/abcode/security/config/JwtService.java
+++ security/src/main/java/com/abcode/security/config/JwtService.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "7A244326462948404D635166546A576E5A7234753778214125442A472D4B614E";
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -42,7 +42,7 @@ public class JwtService {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
 
     }
