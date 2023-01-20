f65d8a65ec164bd150f40402cffad7cd2d9afbab
Arthur Barbosa <arthurbruno03@gmail.com>
2023-01-20 20:43:42 -0300

    Created component Login LoginService and AuthModule

--- security-ui/src/app/app.module.ts
+++ security-ui/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AuthModule } from './auth/auth.module'
+import { AuthModule } from './auth/auth.module';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module'
     BrowserModule,
     AppRoutingModule,
     AuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- security-ui/src/app/auth/auth.module.ts
+++ security-ui/src/app/auth/auth.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from '../components/login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule
-  ]
+    RouterModule,
+    HttpClientModule,
+    LoginRoutingModule
+  ],
+  exports: [RouterModule],
 })
 export class AuthModule { }
